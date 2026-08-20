8d3861012c8124741efdda18854747e16d12d2be
antonio-jsx <antoniodev.1993@gmail.com>
2026-08-20 19:58:44 -0400

    limpieza de archivos

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html className={`${geistSans.variable} ${geistMono.variable} antialiased`} lang="es">
+      <body>{children}</body>
     </html>
   );
 }
